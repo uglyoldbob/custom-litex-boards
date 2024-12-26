30bdfd5577785620e4bcc2012b78e9e7b4721fe7
Thomas Epperson <thomas.epperson@gmail.com>
2024-12-26 09:44:22 -0600

    Change from special to submodule for prbs.

--- uob-litex-boards/targets/sipeed_tang_nano_20k.py
+++ uob-litex-boards/targets/sipeed_tang_nano_20k.py
@@ -126,7 +126,7 @@ class NesInst(LiteXModule):
         self.vout = Endpoint(video_data_layout)
         self.vid_select = CSRStorage(8)
         self.vidtest = PRBS31Generator(24)
-        self.specials += self.vidtest
+        self.submodules += self.vidtest
         self.comb += Case(self.vid_select.storage, {
                 0: [self.vin.ready.eq(self.vout.ready),
                     self.vout.hsync.eq(self.vin.hsync),
